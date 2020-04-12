0169034b8c529c7a453a05a8ace5767c3f1acdb3
Jerusalem01 <3276586184@qq.com>
2020-04-12 20:46:59 +0800

    品牌管理功能

--- tesco-goods/src/main/java/com/jerusalem/goods/controller/BrandController.java
+++ tesco-goods/src/main/java/com/jerusalem/goods/controller/BrandController.java
@@ -38,7 +38,6 @@ public class BrandController {
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = brandService.queryPage(params);
-
         return R.ok().put("page", page);
     }
 
