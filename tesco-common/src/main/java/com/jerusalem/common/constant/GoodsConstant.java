ab59621e662cbcf72963b758cc17f374f0cab3b6
Jerusalem01 <3276586184@qq.com>
2020-04-18 18:35:09 +0800

    抽取商品微服务的常量

--- tesco-common/src/main/java/com/jerusalem/common/constant/GoodsConstant.java
+++ tesco-common/src/main/java/com/jerusalem/common/constant/GoodsConstant.java
@@ -1,12 +1,20 @@
 package com.jerusalem.common.constant;
 
-import lombok.Data;
-
-public class ProductConstant {
-
+/****
+ * @Author: jerusalem
+ * @Description: GoodsConstant
+ * 商品微服务相关常量
+ * @Date 2020/4/16 10:00
+ *****/
+public class GoodsConstant {
 
     public enum  AttrEnum{
+
+        /**
+         * 属性类型
+         */
         ATTR_TYPE_BASE(1,"基本属性"),ATTR_TYPE_SALE(0,"销售属性");
+
         private int code;
         private String msg;
 
