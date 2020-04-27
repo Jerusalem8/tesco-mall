729ff77aafe6263c93fb8eb2369a1727dd8f4983
Jerusalem01 <3276586184@qq.com>
2020-04-27 18:35:27 +0800

    增加枚举常量

--- tesco-common/src/main/java/com/jerusalem/common/constant/WareConstant.java
+++ tesco-common/src/main/java/com/jerusalem/common/constant/WareConstant.java
@@ -1,5 +1,21 @@
 package com.jerusalem.common.constant;
 
+/****
+ * @Author: jerusalem
+ * @Description: GoodsConstant
+ * 仓储微服务相关常量
+ * 采购单的状态：0-新建
+ *             1-已分配
+ *             2-已领取
+ *             3-已完成
+ *             4-有异常
+ * 采购需求的状态：0-新建
+ *               1-已分配
+ *               2-正在采购
+ *               3-采购完成
+ *               4-采购失败
+ * @Date 2020/4/16 10:00
+ *****/
 public class WareConstant {
 
     public enum  PurchaseStatusEnum{
@@ -23,10 +39,9 @@ public class WareConstant {
         }
     }
 
-
     public enum  PurchaseDetailStatusEnum{
         CREATED(0,"新建"),ASSIGNED(1,"已分配"),
-        BUYING(2,"正在采购"),FINISH(3,"已完成"),
+        BUYING(2,"正在采购"),FINISH(3,"采购完成"),
         HASERROR(4,"采购失败");
         private int code;
         private String msg;
