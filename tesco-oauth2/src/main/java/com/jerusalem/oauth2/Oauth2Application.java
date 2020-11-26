6f3fe8274dcd75cae46204079b35874b8a4da864
Jerusalem01 <3276586184@qq.com>
2020-11-26 15:04:15 +0800

    隐藏关键信息

--- tesco-oauth2/src/main/java/com/jerusalem/oauth2/Oauth2Application.java
+++ tesco-oauth2/src/main/java/com/jerusalem/oauth2/Oauth2Application.java
@@ -21,28 +21,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  *  1）、配置全局的逻辑删除规则（省略）
  *  2）、配置逻辑删除的组件Bean（省略）
  *  3）、给Bean加上逻辑删除注解@TableLogic
- *
- * 3、JSR303
- *   1）、给Bean添加校验注解:javax.validation.constraints，并定义自己的message提示
- *   2)、开启校验功能@Valid
- *      效果：校验错误以后会有默认的响应；
- *   3）、给校验的bean后紧跟一个BindingResult，就可以获取到校验的结果
- *   4）、分组校验（多场景的复杂校验）
- *         1)、	@NotBlank(message = "品牌名必须提交",groups = {AddGroup.class,UpdateGroup.class})
- *          给校验注解标注什么情况需要进行校验
- *         2）、@Validated({AddGroup.class})
- *         3)、默认没有指定分组的校验注解@NotBlank，在分组校验情况@Validated({AddGroup.class})下不生效，只会在@Validated生效；
- *
- *   5）、自定义校验
- *      1）、编写一个自定义的校验注解
- *      2）、编写一个自定义的校验器 ConstraintValidator
- *      3）、关联自定义的校验器和自定义的校验注解
- *      @Documented
- *      @Constraint(validatedBy = { ListValueConstraintValidator.class【可以指定多个不同的校验器，适配不同类型的校验】 })
- *      @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
- *      @Retention(RUNTIME)
- *      public @interface ListValue {}
- *
  * 4、统一的异常处理
  * @ControllerAdvice
  *  1）、编写异常处理类，使用@ControllerAdvice。
